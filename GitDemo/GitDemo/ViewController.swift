03dfca973ee86eab6bff719ae66c413a9d802bb5
Marlon Leng <marlonleng@leng.local>
2017-09-08 01:40:19 +0800

    app commit

---
+++ GitDemo/GitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Marlon Leng on 2017/9/8.
+//  Copyright © 2017年 Marlon Leng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
